74cdbfba91fda822e11be8e4e37a492566e2d402
Dmitriy <103449961+bonesones@users.noreply.github.com>
2023-03-03 18:31:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -20,14 +20,17 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 
 
-io.on("connection",(client)=>{
+io.on("connection",function(client) {
     console.log('New websocket connection');
+    this.address = client.handshake.address
  client.on("new user", (username) => {
+    this.address = client.handshake.address;
     client.username = username
-    io.emit('messageFromServer', username + " присоединился к серверу")
+    io.emit('messageFromServer', username + " присоединился к чату")
  })
  client.on('messageFromClient', msg => {
-    io.emit('messageFromServer', client.username + ": " + msg);
+    const prefix = client.handshake.address === this.address ? client.username : "Вы"
+    io.emit('messageFromServer', prefix + ": " + msg);
   });
 
    client.on('disconnect', () => {
